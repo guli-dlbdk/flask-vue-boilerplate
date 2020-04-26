bdd218adf160ecd1d911f49bfe8f0fe25c278950
guli-dlbdk <glstndalbudak@gmail.com>
2020-04-26 18:32:16 +0300

    login page sidebar created

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,8 @@ app = Flask(__name__,
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('heart.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
